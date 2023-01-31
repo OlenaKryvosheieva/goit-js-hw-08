7c130c130d8a30ba35327b0d7ec0be26cfb6c157
OlenaKryvosheieva <112905320+OlenaKryvosheieva@users.noreply.github.com>
2023-01-31 15:19:54 +0200

    Update_task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,17 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const formData = {};
 const inputEl = document.querySelector('.feedback-form input');
 const textareaEl = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
+const savedForm = localStorage.getItem(STORAGE_KEY);
+const parsedForm = JSON.parse(savedForm);
+const formData = parsedForm || { email: '', message: '' };
+
 preFillForm();
 
 formEl.addEventListener('input', throttle(handlInput, 500));
-
 formEl.addEventListener('submit', handlSubmit);
 
 function handlInput(e) {
@@ -19,9 +21,6 @@ function handlInput(e) {
 }
 
 function preFillForm() {
-  const savedForm = localStorage.getItem(STORAGE_KEY || '');
-  const parsedForm = JSON.parse(savedForm);
-
   if (parsedForm) {
     inputEl.value = parsedForm.email;
     textareaEl.value = parsedForm.message;
@@ -31,8 +30,13 @@ function preFillForm() {
 function handlSubmit(e) {
   e.preventDefault();
 
+  if (inputEl.value === '' || textareaEl.value === '') {
+    alert('Будь ласка, заповніть всі поля');
+    return;
+  }
+
   console.log(formData);
 
-  localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
